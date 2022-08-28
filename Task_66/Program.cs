3ebb88fa2963d5ba711ed096e7b0b36bff8e9cdb
Svetlana <bulatova44@gmail.com>
2022-08-28 17:13:15 +0300

    The final version of the task solution

--- Task_66/Program.cs
+++ Task_66/Program.cs
@@ -7,33 +7,50 @@
 
 //1. Решение 1, с помощью цикла.
 
-int rangeOfNumbers (int num1, int num2)
-{   string str = String.Empty;
-    int sum = 0;
-    int number = num1;
-    while (number <= num2)
-    {
-        str = $"{str} {number}";
-        sum = sum + number;
-        number++;       
-    }
-    System.Console.WriteLine($"Были определены числа:{str}. Сумма натуральных элементов в промежутке от {num1} до {num2} равна: ");
-    return sum;   
-}
 
+int rangeOfNumbers (int num1, int num2)
+{
+    string answer = string.Empty;
+    int sum = 0;
+    int number1 = num1;
+    int number2 = num1;
+
+    if (num1 >= num2)
+    {
+        while (number1 >= num2)
+        {
+            answer = $"{answer}{number1} ";
+            sum = sum + number1;
+            number1--;
+        }
+    }
+    else if (num1 <= num2)
+    {
+        while (number1 <= num2)
+        {
+            answer = $"{answer}{number1} ";
+            sum = sum + number1;
+            number1++;
+        }
+    }
+    
+    System.Console.Write($"Определены числа: {answer}");
+    System.Console.WriteLine();
+    System.Console.WriteLine($"Сумма натуральных элементов в промежутке от {num1} до {num2} равна: ");
+    return sum; 
+}
 
 System.Console.WriteLine($"Введите первое число М: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine($"Введите второе число N: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 
-
-//System.Console.Write($"Сумма натуральных элементов в промежутке от M до N равна: ");
 System.Console.WriteLine(rangeOfNumbers(number1, number2));
 System.Console.WriteLine();
 
 
 //2. Вариант решения с рекурсией
+
 
 int rangeOfNaturalNumbers (int numStart, int numEnd)
 {
@@ -42,10 +59,14 @@ int rangeOfNaturalNumbers (int numStart, int numEnd)
     else return 0;
 }
 
+System.Console.WriteLine($"Введите два целых числа, первое меньше второго.");
+System.Console.WriteLine();
 System.Console.WriteLine($"Введите первое число M: ");
 int startNum = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine($"Введите второе число N: ");
 int endtNum = Convert.ToInt32(Console.ReadLine());
 
-System.Console.Write($"Сумма натуральных элементов в промежутке от M до N равна: ");
+System.Console.WriteLine();
+System.Console.WriteLine($"Сумма натуральных элементов в промежутке от {startNum} до {endtNum} равна: ");
 System.Console.WriteLine(rangeOfNaturalNumbers(startNum, endtNum));
+
--- Task_68/Program.cs
+++ Task_68/Program.cs
@@ -4,7 +4,7 @@
 //m = 2, n = 3 -> A(m,n) = 29
 //Для положительных чисел функция Аккермана вычислаяется: А(m,n) = A(m-1, A(m, n-1))
 
-/*
+
 int AckermannFunction (int numM, int numN)
 {
     if (numM == 0)
@@ -21,45 +21,11 @@ int AckermannFunction (int numM, int numN)
     }
 }
 
-long AckermannFunction (long numM, long numN)
-{
-    if (numM == 0)
-    { 
-        return numN + 1;
-    }
-    else if (numM > 0 && numN == 0)
-    {
-        return AckermannFunction(numM - 1, 1);
-    }
-    else
-    {
-        return AckermannFunction(numM - 1, AckermannFunction(numM, numN - 1));   
-    }
-}
-*/
-
-
-
-ulong AckermannFunction (ulong numM, ulong numN)
-{
-    if (numM == 0)
-    { 
-        return numN + 1;
-    }
-    else if (numM > 0 && numN == 0)
-    {
-        return AckermannFunction(numM - 1, 1);
-    }
-    else
-    {
-        return AckermannFunction(numM - 1, AckermannFunction(numM, numN - 1));   
-    }
-}
-
 System.Console.WriteLine($"Введите первое число M: ");
-ulong startNum = Convert.ToUInt64(Console.ReadLine());
+int startNum = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine($"Введите второе число N: ");
-ulong endtNum = Convert.ToUInt64(Console.ReadLine());
+int endtNum = Convert.ToInt32(Console.ReadLine());
 
 System.Console.Write($"Значение функции Аккермана равно: ");
 System.Console.WriteLine(AckermannFunction(startNum, endtNum));
+
